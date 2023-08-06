3a79fc72d0f90fc41eb9624f57764c12df1884fb
Agusramossegovia <agustinaramossegovia@gmail.com>
2023-08-06 13:03:52 -0300

    form netlify

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -3,7 +3,35 @@ import './contacto.css'
 export default function Contacto (){
     return(
         <>
+            <h1>Sumate a la organizacion</h1>
+            <form name="contacto" data-netlify="true" netlify-honeypot="boto-field" hidden>
+                <input type='text' name='nombre'/>
+                <input type='email' name='email'/>
+                <textarea name='comentario'></textarea>
+                
+            </form>
+
+
+            <form method='post' className='formulario'>
+                <label>
+                    Nombre:
+                    <input type='text' name='nombre'/>
+                </label>
+                <label>
+                    Mail:
+                    <input type='email' name='email'/>
+                </label>
+                <label>
+                    Comentarios:
+                    <textarea name='comentario'></textarea>
+                </label>
+
+                <input type="hidden" name="form-name" value="contacto" />
+
+                <button type='submit'>Enviar</button>
+            </form>
         <br/>
+
         <h2>ARGENTINA</h2>
             <p>Cuidad Autonoma de Buenos Aires
             <br/>
