465fe766a415fb25749b77f1d4d5591f61dd7f0d
Agusramossegovia <agustinaramossegovia@gmail.com>
2023-08-13 18:30:17 -0300

    Galeria

--- app/informacion/Swiper.js
+++ app/informacion/Swiper.js
@@ -2,34 +2,37 @@
 
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-//import Slider from 'react-slick';
+import 'swiper/css';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { EffectFade } from 'swiper/modules';
+import { Autoplay} from 'swiper/modules';
 
-function Carousel() { 
+function Slider() { 
   return (
     <Swiper
-    modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay ]}
     spaceBetween={50}
-    effect="fade"
+    effect='AutoPlay'
     slidesPerView={1}
     navigation
     pagination={{ clickable: true }}
     scrollbar={{ draggable: true }}
     >
         <SwiperSlide>
-          <img src='../images/ballenaconbebe.jpg'alt="" style={{width: "75%", height:"100vh"}}/>
+          <img src='../images/ballena1.jpg'alt="" style={{width: "75%", height:"100vh"}}/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src='../images/ballenamusculus.jpg' alt="" style={{width: "100%", height:"100vh"}}/>
+          <img src='../images/ballena2.jpg' alt="" style={{width: "100%", height:"100vh"}}/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src='../images/ballenaorca.jpg' alt="" style={{width: "100%", height:"100vh"}}/>
+          <img src='../images/ballenas3.jpg' alt="" style={{width: "100%", height:"100vh"}}/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src='../images/ballenasolitaria.jpg'alt="" style={{width: "100%", height:"100vh"}}/>
+          <img src='../images/ballena4.jpg'alt="" style={{width: "100%", height:"100vh"}}/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='../images/ballena5.jpg'alt="" style={{width: "100%", height:"100vh"}}/>
         </SwiperSlide>
     </Swiper>
-  );
-};
-export default Carousel
+  )
+}
+export default Slider 
--- app/informacion/page.js
+++ app/informacion/page.js
@@ -1,5 +1,5 @@
 import './informacion.css'
-import Swiper from './Swiper'
+import Slider from './Swiper'
 
 export default function Informacion () {
     return(
@@ -10,14 +10,13 @@ export default function Informacion () {
                 <h2>¿Porque son tan importantes para el ambiente?</h2>
             <p>Son fertilizadoras del océano, mitigadoras del cambio climático y promovedoras de biodiversidad
             <br/>
-            <Swiper/>
+            <Slider/>
             <br/> 
             El rol de las ballenas en el correcto funcionamiento del ecosistema marino es indiscutible. Sus heces ricas en hierro, nitrógeno y otros nutrientes, actúan como fertilizante, aumentando la productividad de pequeñas algas conocidas como fitoplancton. Éstas son el principal alimento de pequeños crustáceos conocidos como kril, que a su vez sostienen la vida de cientos de especies de peces, aves y mamíferos marinos, incluídas las ballenas. Estos eventos de fertilización también pueden facilitar la mitigación de los impactos negativos del cambio climático.
             </p>
         <h3>Especies en peligro de extision</h3>
             <p>Las ballenas estan en peligro de extision, algunas especies mas que otras. Ocho de las trece grandes especies de ballenas siguen en peligro de extinción, son vulnerables, después de décadas de protección. Según datos de la "Lista Roja de especies amenazadas", elaborada por IUCN que nos permite conocer el estado de la amenaza de las especies de animales y plantas en el mundo</p>
-            <img src="../images/ballenasolitaria.jpg"/>
-            <h3>Posibles soluciones</h3>
+        <h3>Posibles soluciones</h3>
             <p>Educacion ambiental, limpieza de las playas, concientizar sobre el cuidado de los oceanos, ect ect. </p>
     </>
    )
